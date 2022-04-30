2508dcd9a5f39e2b91a7c628cef4f013b352b31f
Fi-o-na <98526940+Fi-o-na@users.noreply.github.com>
2022-04-30 10:56:01 +0800

    Add files via upload

---
+++ 2112080208_李增晶/Chapter02/Chapter07/T7_12.cpp
@@ -0,0 +1,17 @@
+/*
+ * FileName: T7_12.cpp
+ * Author:   Zengjing Li
+ * E-mail:   3309726571@qq.com
+ * Date:     Apr 24th, 2022
+ * College:  School of Computer Science and Information Engineering
+ * Function: The problem of one-dimensional array objects.
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
